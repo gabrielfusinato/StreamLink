8899636007f110b07b6ac503f0b551408f910d3d
Gabriel Fusinato <gabrielfusinato92@gmail.com>
2026-04-26 23:40:00 +0000

    Implement auto-generated alias and duplicate link check

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from supabase import create_client
 import os
 from dotenv import load_dotenv
 
+import random
+
 load_dotenv()
 
 url = os.getenv("SUPABASE_URL")
@@ -9,7 +11,47 @@ key = os.getenv("SUPABASE_PUBLISHABLE_KEY")
 
 supabase = create_client(url, key)
 
-response = supabase.table("url").select("*").execute()
+def check_link_in_database(check_url):
+    response = supabase.table("url").select("*").eq("long_url", check_url).execute()
 
-print(response.data)
+    if response.data:
+        return response.data[0]
+     
+    return None
 
+def store_long_url(long_url):
+
+    existing_link = check_link_in_database(long_url)
+
+    if existing_link:
+        return existing_link['tinyAlias']
+    
+    tiny_alias = eeeeeeeeeeeeegenerate_tiny_alias()
+
+    data = {
+        "long_url" : long_url,
+        "tiny_alias" : tiny_alias
+    }
+
+    try:
+        response = supabase.table("url").insert(data).execute()
+        print(f"URL <{response.data}> guardada com sucesso!")
+        return tiny_alias
+
+    except Exception as e:
+        print(f"Erro ao salvar no banco: {e}")
+        raise
+
+def generate_tiny_alias():
+    characters = "STREAMLINKstreamlink1234567890"
+
+    tiny_alias = ''.join(random.choice(characters) for _ in range(10))
+
+    return tiny_alias
+
+
+
+
+if __name__ == "__main__":
+    long_url = str(input("Enter your link: "))
+    store_long_url(long_url)
